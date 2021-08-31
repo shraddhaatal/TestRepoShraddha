6c94f35cf06cfa9d915ee3674374603ea42cbdc2
Shraddha Rathi <shraddhaatal@gmail.com>
2021-08-31 23:28:12 +0400

    branchB changes in Class1

--- src/test/Class1.java
+++ src/test/Class1.java
@@ -2,6 +2,7 @@ package test;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("Hello There");
+		System.out.println("Hello There456");
+		System.out.println("new line for branch B");
 	}
 }
